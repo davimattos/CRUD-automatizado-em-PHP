20a9c9d48cb66d9ee222413ef4a8103073a93a71
davi-miza <davi.mattos@sou.unifeob.edu.br>
2018-09-04 22:19:23 -0300

    exemplos de curl(clonagem de código)

--- curl/curlGetRequest.php
+++ curl/curlGetRequest.php
@@ -3,7 +3,7 @@
 $ch = curl_init();
 
 //informar URL e outras funções ao CURL
-curl_setopt($ch, CURLOPT_URL, "http://www.joogle.com.br/");
+curl_setopt($ch, CURLOPT_URL, "http://www.google.com/testehghgf");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 //acessar URL
